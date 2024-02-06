8b1d59b8661f9e93acc73bbe326f15e7d6d2b3c4
ValeriZaitsev <107877062+ValeriZaitsev@users.noreply.github.com>
2024-02-06 21:58:22 +0200

    second commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const loginForm = document.getElementById("loginForm");
+const userInput = document.getElementById("userInput");
+const passwordInput = document.getElementById("password");
+const loginButoon = document.getElementById("loginButton");
+
